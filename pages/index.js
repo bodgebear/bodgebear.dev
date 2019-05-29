5a1461a1d0b401203d9615374b496d525ec3f7c9
ST <direct6991@gmail.com>
2019-05-29 18:52:46 +0100

    font

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import GlobalStyles from "../utils/GlobalStyles";
+
+
+const index = () => {
+  return (
+    <GlobalStyles>
+    <React.Fragment>
+      Heello
+    </React.Fragment>
+    </GlobalStyles>
+      )
+};
+
+export default index;
---
+++ utils/GlobalStyles.js
@@ -0,0 +1,10 @@
+import {createGlobalStyle} from "styled-components";
+import Miners from 'Pixel-Miners.otf';
+
+const GlobalStyles = createGlobalStyle`
+  html {
+   background-color: red;
+  }
+`;
+
+export default GlobalStyles;
