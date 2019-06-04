2ebc7f2046d95f894e0e9a152da26891a3f09e1f
ST <direct6991@gmail.com>
2019-06-04 18:32:50 +0100

    StyleLint

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
-// import bgImg from '../static/background.png'
-// import headerText from '../static/bb_name.png'
-// import bear from '../static/bear.png';
-// import desk from '../static/desk2.png'
-import banner from '../static/banner_bear.png';
+import HeaderText from '../static/bb_name.png';
+import BannerBear from '../static/banner_bear_no_logo.png';
 
+const StyledHeader = styled('header')`
+  width: 100vw;
+  position: relative;
+`;
+
+const StyledText = styled('img')`
+  height: 2.5rem;
+  position: absolute;
+  top: 1rem;
+  left: 50%;
+  transform: translateX(-50%);
+`;
 
 const StyledBanner = styled('img')`
-  height: 20rem;
+  height: 15rem;
   image-rendering: pixelated;
-
 `;
 
 
 const Header = () => (
-  // eslint-disable-next-line react/jsx-filename-extension
-  <StyledBanner src={banner} />
+  <StyledHeader>
+    <StyledText src={HeaderText} />
+    <StyledBanner src={BannerBear} />
+  </StyledHeader>
 );
 
 export default Header;
