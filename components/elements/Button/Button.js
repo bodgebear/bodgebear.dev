b8f4618c5ff5d026cb52a15873eef1d72d4ddf8c
ST <direct6991@gmail.com>
2019-06-06 18:40:26 +0100

    FIxed button

--- components/elements/Button/Button.js
+++ components/elements/Button/Button.js
@@ -1,29 +1,40 @@
 import styled from 'styled-components';
-import colors from '../../styles/colors';
+import colors from '../../../styles/colors';
+
+const utils = {
+  width: '0.25em',
+  opacity: 0.6,
+  color: '#FFFFFF',
+  bgColor: '#FFFFFF',
+  hoverColor: '#151616',
+  hoverBgColor: '#FFFFFF',
+  disabledBgColor: 'rgba(0, 0, 0, 0.2)',
+  disabledColor: 'rgba(255,255,255, 0.7)',
+};
 
 const Button = styled('button')`
   border: none;
   padding: 0.6em 1.4em;
-  background-color: ${colors.black};
+  background-color: ${colors.dark};
   color: ${colors.white};
   box-shadow:
-    -0.25em 0 0 rgba(255, 255, 255, 0.6),
-    0.25em 0 0 rgba(255, 255, 255, 0.6),
-    0 0.25em 0 rgba(255, 255, 255, 0.6),
-    0 -0.25em 0 rgba(255, 255, 255, 0.6);
+    -${utils.width} 0 0 rgba(255, 255, 255, ${utils.opacity}),
+    ${utils.width} 0 0 rgba(255, 255, 255, ${utils.opacity}),
+    0 ${utils.width}  0 rgba(255, 255, 255, ${utils.opacity}),
+    0 -${utils.width} 0 rgba(255, 255, 255, ${utils.opacity});
   font-size: 1rem;
-  transition: all 0.1s;
+  transition: all 0.1s ease-in-out;
+  transition-property: color, background-color;
   font-family: 'Miners', serif;
 
   &:hover {
-    background-color: ${colors.white};
-    color: ${colors.black};
+    background-color: ${utils.hoverBgColor};
+    color: ${utils.hoverColor};
   }
 
-  &:disabled,
-  [disabled=disabled] {
-    background-color: rgba(${colors.black}, 0.2);
-    color: rgba(255, 255, 255, 0.7);
+  &:disabled {
+    background-color: ${utils.disabledBgColor};
+    color: ${utils.disabledColor};
   }
 `;
 
