a0c07d63f0c2a9630735b5f93aee517f0ea2b72a
Bartosz Legięć <bartosz@legiec.eu>
2019-06-16 00:46:51 +0200

    Remove next-images package, add own config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,33 @@
 const withFonts = require('next-fonts');
-const withImages = require('next-images');
 const withPlugins = require('next-compose-plugins');
 
 module.exports = withPlugins(
-  [withImages, withFonts],
+  [
+    withFonts,
+  ],
   {
+    webpack(config, options) {
+      const { isServer } = options;
+      const assetPrefix = '';
+
+      config.module.rules.push({
+        test: /\.(jpe?g|png|svg|gif|ico|webp)$/,
+        use: [
+          {
+            loader: require.resolve('url-loader'),
+            options: {
+              limit: false,
+              fallback: require.resolve('file-loader'),
+              publicPath: `${assetPrefix}/_next/static/images/`,
+              outputPath: `${isServer ? '../' : ''}static/images/`,
+              name: '[name]-[hash].[ext]',
+            },
+          },
+        ],
+      });
+
+      return config;
+    },
     target: 'serverless',
   },
 );
