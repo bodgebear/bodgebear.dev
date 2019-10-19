42c099d5eeebe1159c16b721aea38d179d164cec
Bartosz Legięć <bartosz@legiec.eu>
2019-10-20 01:04:09 +0200

    Remove container from app

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,4 +1,4 @@
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import Head from 'next/head';
 import React from 'react';
 
@@ -15,14 +15,13 @@ export default class MyApp extends App {
 
   render() {
     const { Component, pageProps } = this.props;
-
     return (
-      <Container>
+      <>
         <Head>
           <title>Bodging Bear</title>
         </Head>
         <Component {...pageProps} />
-      </Container>
+      </>
     );
   }
 }
